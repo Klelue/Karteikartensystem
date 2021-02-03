96eb01034ed0c71c50bacf191c17bcc2787f54a0
ITBH\s.bruns <stephant.bruns@gmail.com>
2021-02-03 22:47:15 +0100

    Add methods to StapelController

--- src/Karteikartensystem/1. Code/Controller/StapelController.cs
+++ src/Karteikartensystem/1. Code/Controller/StapelController.cs
@@ -1,5 +1,8 @@
-﻿using KartenRepository;
+﻿using System.Collections.Generic;
+using System.Linq;
+using KartenRepository;
 using Repositories;
+using Stapel;
 
 namespace Controller
 {
@@ -13,9 +16,86 @@ namespace Controller
             kartenRepository = new KarteRepository();
         }
 
-        public void AddAlleKartenToStapelAuswahl()
+
+        public List<Stapel.Stapel> GetAlleStapel()
         {
-            //stapelRepository.GetAlleStapel()
+            return stapelRepository.GetAlleStapel().ToList();
+        }
+
+        public List<Stapel.Stapel> GetAuswahlStapel(int stapelId)
+        {
+            return stapelRepository.GetAlleStapel().Where(Stapel => Stapel.Id == stapelId).ToList();
+        }
+
+        public List<Karte> GetAlleStapelKarten(int stapelId)
+        {
+            return kartenRepository.GetKarten(stapelId).ToList();
+        }
+
+        private void AddAlleKartenToStapel(Stapel.Stapel stapel)
+        {
+            stapel.Karten = GetAlleStapelKarten(1);
+        }
+
+        public bool AddNewLeerStapel(string name)
+        {
+            Stapel.Stapel stapel = new Stapel.Stapel();
+            stapel.Name = name;
+            //TODO
+            //DB REQUEST ADD STAPEL SOLL RETURN DB ID TO STAPEL OBJEKT ERMÖGLICHEN
+            return stapelRepository.AddStapel(stapel);
+        }
+
+        public void AddNewKarteToStapel(Karte karte, int id)
+        {
+
+        }
+
+        /// <summary>
+        /// Ersetzt UebersichMethoden.cs
+        /// </summary>
+        /// <param name="suche"></param>
+        /// <param name="suchziel"></param>
+        /// <returns></returns>
+        public List<Stapel.Stapel> GetSuchergebnisStapelList(string suche, List<Stapel.Stapel> suchziel)
+        {
+            List<Stapel.Stapel> suchergebnis = new List<Stapel.Stapel>();
+            suche = suche.ToLower();
+
+            foreach (Stapel.Stapel stapel in suchziel)
+            {
+                string stapelNameLower = stapel.Name.ToLower();
+
+                if (stapelNameLower.Contains(suche))
+                {
+                    suchergebnis.Add(stapel);
+                }
+            }
+
+            return suchergebnis;
+        }
+        /// <summary>
+        /// Ersetzt UebersichMethoden.cs
+        /// </summary>
+        /// <param name="suche"></param>
+        /// <param name="suchziel"></param>
+        /// <returns></returns>
+        public List<string> GetSuchergebnisStringList(string suche, List<string> suchziel)
+        {
+            List<string> suchergebnis = new List<string>();
+            suche = suche.ToLower();
+
+            foreach (string stapel in suchziel)
+            {
+                string stapelLower = stapel.ToLower();
+
+                if (stapelLower.Contains(suche))
+                {
+                    suchergebnis.Add(stapel);
+                }
+            }
+
+            return suchergebnis;
         }
     }
 }
