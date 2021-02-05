4a2a775b68b0a7e2a12b7b08de046817ca33cd2b
Klemens Lünert <kluenert@web.de>
2021-02-05 16:08:18 +0100

    Update KartenUebersicht

--- src/Karteikartensystem/1. Code/Ansicht/WindowsFormsApp1/Fenster/KartenUebersicht.cs
+++ src/Karteikartensystem/1. Code/Ansicht/WindowsFormsApp1/Fenster/KartenUebersicht.cs
@@ -20,71 +20,10 @@ namespace AnsichtsFenster.Fenster
 
             KarteRepository repository = new KarteRepository();
             alleKarten = repository.GetAlleKartenEinesStapels(stapelId).ToList();
-            ListViewFormatieren();
             selectedKarte = alleKarten[0];
             lbl_FrageSetzen();
         }
-
-        private void ListViewFormatieren()
-        {
-            listView_KartenAnzeige.View = View.Details;
-            listView_KartenAnzeige.Columns.Add("Fragen");
-            KartenAnzeigen(alleKarten);
-        }
-
-        private void KartenAnzeigen(List<Karte> anzeigeList)
-        {
-            List<ListViewItem> listViewItem = new List<ListViewItem>();
-
-            foreach (Karte karte in anzeigeList)
-            {
-                listViewItem.Add(new ListViewItem(karte.Frage));
-            }
-
-            listView_KartenAnzeige.Items.Clear();
-            listView_KartenAnzeige.Items.AddRange(listViewItem.ToArray());
-        }
-
-        private void txt_KartenSuche_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Enter)
-            {
-
-                if (txt_KartenSuche.Text.Trim() == "")
-                {
-                    KartenAnzeigen(alleKarten);
-                }
-
-                else
-                {
-                    KartenSucheAnzeigen(txt_KartenSuche.Text);
-                }
-
-                txt_KartenSuche.Clear();
-            }
-        }
-
-        private void KartenSucheAnzeigen(string suchbegriff)
-        {
-            List<Karte> ergebnisListe = StringSuchTool.GetSuchergebnis(suchbegriff, alleKarten);
-
-            if (ergebnisListe.Count < 1)
-            {
-                MessageBox.Show("Leider kein Eintrag gefunden", "Sorry", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                KartenAnzeigen(alleKarten);
-            }
-
-            else
-            {
-                KartenAnzeigen(ergebnisListe);
-            }
-        }
-
-        private void ListViewColumnClick(object sender, ColumnClickEventArgs e)
-        {
-            this.listView_KartenAnzeige.ListViewItemSorter = new ListViewItemComparer(e.Column);
-        }
-
+        
         private void btn_home_Click(object sender, EventArgs e)
         {
             base.Dispose();
@@ -125,11 +64,11 @@ namespace AnsichtsFenster.Fenster
         {
             if (alleKarten.Count > 1)
             {
-                int aktuellerId = selectedKarte.Id;
+                int aktuellerIdex = alleKarten.IndexOf(selectedKarte);
 
-                if (aktuellerId < alleKarten.Count)
+                if ( aktuellerIdex +1 < alleKarten.Count)
                 {
-                    selectedKarte = alleKarten[aktuellerId];
+                    selectedKarte = alleKarten[aktuellerIdex+1];
                 }
 
                 else
@@ -147,15 +86,5 @@ namespace AnsichtsFenster.Fenster
             richTxt_Antwort.Clear();
         }
 
-        private Karte SelectedKarteAsKarte(string karteFrage)
-        {
-            return alleKarten.Find(karte => karte.Frage == karteFrage);
-        }
-
-        private void listView_KartenAnzeige_MouseClick(object sender, MouseEventArgs e)
-        {
-            selectedKarte = SelectedKarteAsKarte(listView_KartenAnzeige.SelectedItems[0].Text);
-            lbl_FrageSetzen();
-        }
     }
 }
