4b998e924c89bb4a2de7257b59c10f3c67b3a4e0
Klemens Lünert <kluenert@web.de>
2021-02-08 15:07:10 +0100

    Add ChallangeView

---
+++ src/Karteikartensystem/1. Code/Ansicht/WindowsFormsApp1/Fenster/ChallengeView.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using AnsichtsFenster.Utilities;
+using System.Windows.Forms;
+using Model;
+using Repositories;
+
+namespace AnsichtsFenster.Fenster
+{
+    public partial class ChallengeView : Form
+    {
+        private Karte selectedKarte;
+        private KarteRepository repository;
+        private List<Karte> alleKarten;
+        private Stoppuhr stoppuhr;
+        private int richtigeAntworten;
+        private int falscheAntworten;
+        private readonly int abschlussZeit;
+
+        public ChallengeView(int time, int anzahl, int stapelId)
+        {
+            InitializeComponent();
+            repository = new KarteRepository();
+            alleKarten = repository.GetAlleKartenEinesStapels(stapelId).ToList();
+
+            if (alleKarten.Count >= 1)
+            {
+                selectedKarte = alleKarten[0];
+            }
+            else // falls keine Karten vorhanden
+            {
+                Karte karte = new Karte();
+                karte.Frage = "Leider ist diese Liste leer";
+                karte.Antwort = "Da keine Karten vorhanden sind, \n sind auch keine Antworten vorhanden";
+                selectedKarte = karte;
+            }
+
+            stoppuhr = new Stoppuhr();
+            stoppuhr.Start();
+            lbl_ZeitAngabe.Text = stoppuhr.ToString();
+            lbl_Auswertung.Visible = false;
+            FrageSetzen();
+            
+        }
+
+        private void IsCorrect()
+        {
+            var checkedRadioButton = pnl_FrageAntwort.Controls.OfType<RadioButton>().FirstOrDefault(x => x.Checked == true);
+            if (selectedKarte.Antwort == checkedRadioButton.Text)
+            {
+                richtigeAntworten++;
+            }
+            else
+            {
+                falscheAntworten++;
+            }
+        }
+
+
+        private void FrageSetzen()
+        {
+            radioButtonAntwort1.Checked = true;
+
+
+            if (selectedKarte == null)  //|| stoppuhr.GetZeit() <= abschlussZeit)
+            {
+                Auswertung();
+            }
+
+            else
+            {
+                lbl_Frage.Text = selectedKarte.Frage;
+               
+                RadioButtonsAnlegen();
+            }
+        }
+
+        private void RadioButtonsAnlegen()
+        {
+            List<string> antworten = RandomAntwortenAnordnung();
+            radioButtonAntwort1.Text = antworten[0];
+            radioButtonAntwort2.Text = antworten[1];
+            radioButtonAntwort3.Text = antworten[2];
+            radioButtonAntwort4.Text = antworten[3];
+
+        }
+
+        private List<string> RandomAntwortenAnordnung()
+        {
+            List<string> antworten = new List<string>();
+            antworten.Add(selectedKarte.Antwort);
+           // antworten.Add(selectedKarte.FakeAntwort1);
+           // antworten.Add(selectedKarte.FakeAntwort2);
+           // antworten.Add(selectedKarte.FakeAntwort3);
+
+           antworten.Add("test1");
+           antworten.Add("test2");
+           antworten.Add("test3");
+
+           Random random = new Random();
+
+           for (int i = 0; i < antworten.Count; i++)
+           {
+               int j = random.Next(antworten.Count);
+               string antwort = antworten[j];
+               antworten[j] = antworten[i];
+               antworten[i] = antwort;
+           }
+
+            return antworten;
+        }
+
+        private void btn_Next_Click(object sender, EventArgs e)
+        {
+            NaechsteKarte();
+            FrageSetzen();
+        }
+
+        private void NaechsteKarte()
+        {
+
+            IsCorrect();
+            int indexAktuell = alleKarten.IndexOf(selectedKarte);
+            if (indexAktuell+1 < alleKarten.Count)
+            {
+                selectedKarte = alleKarten[indexAktuell + 1];
+            }
+            else
+            {
+                selectedKarte = null;
+            }
+        }
+
+        private void btn_finish_Click(object sender, EventArgs e)
+        {
+            IsCorrect();
+            Auswertung();
+        }
+
+        private void Auswertung()
+        {
+            pnl_FrageAntwort.Visible = false;
+            btn_finish.Visible = false;
+            lbl_Auswertung.Visible = true;
+            
+            
+            lbl_Auswertung.Text = "Richtige Antworten: " + richtigeAntworten +
+                                  "\nFalsche Antworten: " + falscheAntworten + 
+                                  "\nRichtig in Prozent: "+ ((richtigeAntworten * 100 )/ alleKarten.Count) + "%";
+            
+        }
+    }
+}
--- src/Karteikartensystem/1. Code/Ansicht/WindowsFormsApp1/Fenster/StapelUebersichtView.Designer.cs
+++ src/Karteikartensystem/1. Code/Ansicht/WindowsFormsApp1/Fenster/StapelUebersichtView.Designer.cs
@@ -40,26 +40,25 @@ namespace AnsichtsFenster.Fenster
             this.pnl_btn = new System.Windows.Forms.Panel();
             this.btn_KartenHinzufuegen = new System.Windows.Forms.Button();
             this.btn_Entfernen = new System.Windows.Forms.Button();
+            this.btn_Challenge = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.pnl_btn.SuspendLayout();
             this.SuspendLayout();
             // 
             // txt_StapelSuche
             // 
-            this.txt_StapelSuche.Location = new System.Drawing.Point(23, 89);
-            this.txt_StapelSuche.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_StapelSuche.Location = new System.Drawing.Point(17, 72);
             this.txt_StapelSuche.Name = "txt_StapelSuche";
-            this.txt_StapelSuche.Size = new System.Drawing.Size(407, 22);
+            this.txt_StapelSuche.Size = new System.Drawing.Size(306, 20);
             this.txt_StapelSuche.TabIndex = 0;
             this.txt_StapelSuche.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txt_StapelSuche_KeyDown);
             // 
             // listView_Ausgabe
             // 
             this.listView_Ausgabe.HideSelection = false;
-            this.listView_Ausgabe.Location = new System.Drawing.Point(23, 142);
-            this.listView_Ausgabe.Margin = new System.Windows.Forms.Padding(4);
+            this.listView_Ausgabe.Location = new System.Drawing.Point(17, 115);
             this.listView_Ausgabe.Name = "listView_Ausgabe";
-            this.listView_Ausgabe.Size = new System.Drawing.Size(407, 358);
+            this.listView_Ausgabe.Size = new System.Drawing.Size(306, 292);
             this.listView_Ausgabe.TabIndex = 1;
             this.listView_Ausgabe.UseCompatibleStateImageBehavior = false;
             this.listView_Ausgabe.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.ListViewColumnClick);
@@ -69,10 +68,9 @@ namespace AnsichtsFenster.Fenster
             // lbl_SuchInfo
             // 
             this.lbl_SuchInfo.AutoSize = true;
-            this.lbl_SuchInfo.Location = new System.Drawing.Point(19, 47);
-            this.lbl_SuchInfo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_SuchInfo.Location = new System.Drawing.Point(14, 38);
             this.lbl_SuchInfo.Name = "lbl_SuchInfo";
-            this.lbl_SuchInfo.Size = new System.Drawing.Size(216, 17);
+            this.lbl_SuchInfo.Size = new System.Drawing.Size(162, 13);
             this.lbl_SuchInfo.TabIndex = 2;
             this.lbl_SuchInfo.Text = "Stabelsuche mit Enter bestätigen";
             // 
@@ -81,18 +79,16 @@ namespace AnsichtsFenster.Fenster
             this.panel1.Controls.Add(this.listView_Ausgabe);
             this.panel1.Controls.Add(this.lbl_SuchInfo);
             this.panel1.Controls.Add(this.txt_StapelSuche);
-            this.panel1.Location = new System.Drawing.Point(16, 15);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(461, 524);
+            this.panel1.Size = new System.Drawing.Size(346, 426);
             this.panel1.TabIndex = 3;
             // 
             // btn_StapelHinzufuegen
             // 
-            this.btn_StapelHinzufuegen.Location = new System.Drawing.Point(5, 164);
-            this.btn_StapelHinzufuegen.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_StapelHinzufuegen.Location = new System.Drawing.Point(4, 133);
             this.btn_StapelHinzufuegen.Name = "btn_StapelHinzufuegen";
-            this.btn_StapelHinzufuegen.Size = new System.Drawing.Size(240, 43);
+            this.btn_StapelHinzufuegen.Size = new System.Drawing.Size(180, 35);
             this.btn_StapelHinzufuegen.TabIndex = 4;
             this.btn_StapelHinzufuegen.Text = "Stapel Hinzufügen";
             this.btn_StapelHinzufuegen.UseVisualStyleBackColor = true;
@@ -100,10 +96,9 @@ namespace AnsichtsFenster.Fenster
             // 
             // btn_Statisken
             // 
-            this.btn_Statisken.Location = new System.Drawing.Point(5, 18);
-            this.btn_Statisken.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Statisken.Location = new System.Drawing.Point(4, 15);
             this.btn_Statisken.Name = "btn_Statisken";
-            this.btn_Statisken.Size = new System.Drawing.Size(240, 43);
+            this.btn_Statisken.Size = new System.Drawing.Size(180, 35);
             this.btn_Statisken.TabIndex = 5;
             this.btn_Statisken.Text = "Statistiken";
             this.btn_Statisken.UseVisualStyleBackColor = true;
@@ -115,18 +110,16 @@ namespace AnsichtsFenster.Fenster
             this.pnl_btn.Controls.Add(this.btn_Entfernen);
             this.pnl_btn.Controls.Add(this.btn_StapelHinzufuegen);
             this.pnl_btn.Controls.Add(this.btn_Statisken);
-            this.pnl_btn.Location = new System.Drawing.Point(735, 241);
-            this.pnl_btn.Margin = new System.Windows.Forms.Padding(4);
+            this.pnl_btn.Location = new System.Drawing.Point(551, 196);
             this.pnl_btn.Name = "pnl_btn";
-            this.pnl_btn.Size = new System.Drawing.Size(257, 298);
+            this.pnl_btn.Size = new System.Drawing.Size(193, 242);
             this.pnl_btn.TabIndex = 6;
             // 
             // btn_KartenHinzufuegen
             // 
-            this.btn_KartenHinzufuegen.Location = new System.Drawing.Point(5, 90);
-            this.btn_KartenHinzufuegen.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_KartenHinzufuegen.Location = new System.Drawing.Point(4, 73);
             this.btn_KartenHinzufuegen.Name = "btn_KartenHinzufuegen";
-            this.btn_KartenHinzufuegen.Size = new System.Drawing.Size(240, 43);
+            this.btn_KartenHinzufuegen.Size = new System.Drawing.Size(180, 35);
             this.btn_KartenHinzufuegen.TabIndex = 8;
             this.btn_KartenHinzufuegen.Text = "Karten Editieren";
             this.btn_KartenHinzufuegen.UseVisualStyleBackColor = true;
@@ -134,24 +127,33 @@ namespace AnsichtsFenster.Fenster
             // 
             // btn_Entfernen
             // 
-            this.btn_Entfernen.Location = new System.Drawing.Point(5, 231);
-            this.btn_Entfernen.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Entfernen.Location = new System.Drawing.Point(4, 188);
             this.btn_Entfernen.Name = "btn_Entfernen";
-            this.btn_Entfernen.Size = new System.Drawing.Size(240, 43);
+            this.btn_Entfernen.Size = new System.Drawing.Size(180, 35);
             this.btn_Entfernen.TabIndex = 7;
             this.btn_Entfernen.Text = "Stapel Entfernen";
             this.btn_Entfernen.UseVisualStyleBackColor = true;
             this.btn_Entfernen.Click += new System.EventHandler(this.btn_Entfernen_Click);
             // 
+            // btn_Challenge
+            // 
+            this.btn_Challenge.Location = new System.Drawing.Point(555, 137);
+            this.btn_Challenge.Name = "btn_Challenge";
+            this.btn_Challenge.Size = new System.Drawing.Size(154, 23);
+            this.btn_Challenge.TabIndex = 7;
+            this.btn_Challenge.Text = "Challenge";
+            this.btn_Challenge.UseVisualStyleBackColor = true;
+            this.btn_Challenge.Click += new System.EventHandler(this.btn_Challenge_Click);
+            // 
             // StapelUebersichtView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Challenge);
             this.Controls.Add(this.pnl_btn);
             this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "StapelUebersichtView";
             this.Text = "Stapel Übersicht";
@@ -173,6 +175,7 @@ namespace AnsichtsFenster.Fenster
         private System.Windows.Forms.Panel pnl_btn;
         private System.Windows.Forms.Button btn_Entfernen;
         private System.Windows.Forms.Button btn_KartenHinzufuegen;
+        private System.Windows.Forms.Button btn_Challenge;
     }
 }
 
